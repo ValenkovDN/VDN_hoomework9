de6b843735647350d3440ab7edca2d3049046f75
Дмитрий <rotweis87@gmail.com>
2022-09-25 15:27:03 +0300

    Добавил решение задачи 64

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());;
+NaturalToLow(m, n);
+
+
+void NaturalToLow(int m, int n)
+{
+    if (n > m)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(m, n + 1);
+        Console.Write(n + " ");
+    }
+}
